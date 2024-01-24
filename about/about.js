5df8f4eca379b9a65aa5ee4ce535c3be6f79d5d2
= <=>
2024-01-24 11:29:30 +0300

    Back

--- about/about.js
+++ about/about.js
@@ -1,6 +1,7 @@
 let search_btn = document.getElementById('search_btn')
 let input = document.getElementById('username')
-
+let more = document.getElementById('more')
+let back = document.getElementById('back')
 search_btn.addEventListener('click', searchFunc)
 function searchFunc(){
     let nav = document.querySelector('.nav_bar')
@@ -12,3 +13,9 @@ function searchFunc(){
         search_btn.innerText = 'Search'
     }
 }
+more.addEventListener('click', function(){
+    document.querySelector('header').classList.add('more')
+})
+back.addEventListener('click', function(){
+    document.querySelector('header').classList.remove('more')
+})
